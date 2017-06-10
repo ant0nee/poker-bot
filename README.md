90825c59d3344520f1ad7ce4145e4f8ad9b7a338
Anthony Smith <anthonysmith1057@gmail.com>
2017-06-10 20:02:53 +0100

    created startup files

---
+++ README.md
@@ -0,0 +1,11 @@
+#dependencies
+- discord.js
+- fs
+
+#getting started
+Create a file called "setup.json" with the contents: 
+```{
+	"token": "<PUT TOKEN HERE>"
+}
+```
+Ask ant0nee for the bot token. 
---
+++ bot.js
@@ -0,0 +1,29 @@
+//dependancies 
+const DISCORD = require("discord.js");
+const CLIENT = new DISCORD.Client();
+const FS = require("fs");
+//code
+CLIENT.on('ready', () => {
+
+	console.log(`Logged in as ${CLIENT.user.username}`);
+
+});	
+
+CLIENT.on('message', msg => {
+
+	
+
+});
+
+//get bot token
+FS.readFile('setup.json', function(strError, strData) {
+
+	if (strError) {
+
+		throw strError; 
+
+	}
+
+	CLIENT.login(JSON.parse(strData).token);
+
+});
